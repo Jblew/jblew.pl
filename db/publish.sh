4e1503de0c95cf29c80d3e67d2f2b0e7ba127e8d
Jędrzej Lewandowski <jedrzej@pitchup.com>
2020-03-29 18:20:11 +0000

    ci: fix db publish script

--- db/publish.sh
+++ db/publish.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e # fail on first error
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.." # parent dir of scripts dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/." # parent dir of scripts dir
 cd "${DIR}"
 
-IMAGE_NAME="gcr.io/jblewpl-kube/jblewplold-mysql"
+IMAGE_NAME="gcr.io/jblewpl-kube/jblewplold-db"
 
 docker build --no-cache -t "${IMAGE_NAME}:latest" .
 docker push "${IMAGE_NAME}:latest"
