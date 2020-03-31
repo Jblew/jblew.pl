daa5b651b4947a122a51fa2a255ad0d9d8215b38
Jędrzej Lewandowski <jedrzej@pitchup.com>
2020-03-31 08:42:00 +0000

    fix: php redirection

--- web/www/index.php
+++ web/www/index.php
@@ -1,4 +1,22 @@
 <?php
+
+function redirectTohttps() {
+    if ((isset($_SERVER['HTTPS']) && (($_SERVER['HTTPS'] == 'on') || ($_SERVER['HTTPS'] == '1'))) || (isset($_SERVER['HTTPS']) &&    $_SERVER['SERVER_PORT'] == 443)) {
+        $_SERVER['HTTPS'] = true;
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
+        $_SERVER['HTTPS'] = true;
+    } else {
+        $_SERVER['HTTPS'] = false;
+    }
+
+    if($_SERVER['HTTPS']!="on" && strpos($_SERVER['HTTP_USER_AGENT'], 'Google HC') === false) {
+        $redirect= "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+        header("Location: $redirect");
+        exit();
+    }
+}
+
+
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
